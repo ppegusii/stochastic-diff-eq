facc826d5163c48054ae46d2f9c8fc09cdb924f0
Patrick Pegus II <patrick.pegus@gmail.com>
2014-04-20 00:25:23 -0400

    Tried to speed low pass by inserting into zero matrix. It did not help.

--- code/CompressLowPass.m
+++ code/CompressLowPass.m
@@ -10,6 +10,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function i = CompressLowPass(i,f)
 	sizeI = size(i);
+	%need to figure out how the compression ratio relates to the
+	%diagonal chosen
 	%the row where we begin zeroing
 	zeroRow = floor(f*sizeI(1))+1;
 	%the column where we begin zeroing
@@ -18,11 +20,25 @@ function i = CompressLowPass(i,f)
 	for j=1:colorCnt
 		%dct2 is the matlab 2-d DCT
 		y=dct2(i(:,:,j));
-		for m=zeroRow:sizeI(1)
-			for n=zeroCol:sizeI(2)
-				y(m,n)=0;
+		%need to figure out how the compression ratio relates to the
+		%diagonal chosen
+		%choose which is faster zero out values or
+		%insert values to zero matrix
+		if(false)
+			for m=zeroRow:sizeI(1)
+				for n=zeroCol:sizeI(2)
+					y(m,n)=0;
+				end
 			end
+			i(:,:,j)=idct2(y);
+		else
+			z = zeros(sizeI(1),sizeI(2));
+			for m=1:zeroRow-1
+				for n=1:zeroCol-1
+					z(m,n)=y(m,n);
+				end
+			end
+			i(:,:,j)=idct2(z);
 		end
-		i(:,:,j)=idct2(y);
 	end
 end
