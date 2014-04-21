5ae1df6a7087e1b4a44d8363a332289de430c333
Patrick Pegus II <patrick.pegus@gmail.com>
2014-04-21 11:10:43 -0400

    Corrected Display.m parameters

--- code/Display.m
+++ code/Display.m
@@ -2,6 +2,8 @@
 %	inputs:
 %		i = an image
 %		r = the discrete realized channel rate up to this point in time
+%		f = the compression factor at each time step
+%		t = time
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function Display(i,r)
+function Display(i,r,f,t)
 end
